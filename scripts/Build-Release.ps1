a119585ade92b78dfd5e6f663da1892becc920a6
Justichuu <103864306+Justichuu@users.noreply.github.com>
2026-07-22 17:56:49 -0400

    Fix managed upgrade file-layout validation

--- scripts/Build-Release.ps1
+++ scripts/Build-Release.ps1
@@ -2,7 +2,7 @@
 [CmdletBinding()]
 param(
     [ValidatePattern('^\d+\.\d+\.\d+$')]
-    [string]$Version = '1.3.2',
+    [string]$Version = '1.3.3',
 
     [string]$OutputDirectory = ''
 )
--- src/SwitzerlandVPN.Installer.cs
+++ src/SwitzerlandVPN.Installer.cs
@@ -11,9 +11,9 @@ using System.Windows.Forms;
 [assembly: AssemblyCompany("Justichuu")]
 [assembly: AssemblyProduct("Switzerland VPN")]
 [assembly: AssemblyCopyright("Copyright (c) Justichuu")]
-[assembly: AssemblyVersion("1.3.2.0")]
-[assembly: AssemblyFileVersion("1.3.2.0")]
-[assembly: AssemblyInformationalVersion("1.3.2")]
+[assembly: AssemblyVersion("1.3.3.0")]
+[assembly: AssemblyFileVersion("1.3.3.0")]
+[assembly: AssemblyInformationalVersion("1.3.3")]
 
 namespace SwitzerlandVpn.Installer
 {
